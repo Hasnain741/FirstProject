5cc212548a11c08d7ce390cee5d2843c71d4efea
Hasnain741 <mhasnainali4@gmail.com>
2018-03-06 01:10:13 +0500

    my first commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, TextInput, Button, ActivityIndicator, View } from 'react-native';
+
+export default class App extends React.Component {
+	
+	constructor(props){
+		super(props)
+		this.state = {
+			showActivityIndicator:false,
+		}
+	}
+	
+  render() {
+	const {showActivityIndicator} = this.state
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.textInput}/>
+		<Button title="Press me" onPress={()=>this.setState({showActivityIndicator:true})} style={styles.btn}/>
+			{showActivityIndicator && <ActivityIndicator />}
+		
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+	  width:320,
+	  height:40,
+	  fontSize:20,
+  },
+  btn: {
+	  width:320,
+	  height:40,
+	  fontSize:20,
+  }
+  
+});
